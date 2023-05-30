86a02229f291bfe4e1fa434d5924629b12741e50
frankleitelemoscosta <frankleite090@gmail.com>
2023-05-30 13:32:22 -0300

    Organization the project

--- src/main.c
+++ src/main.c
@@ -1,12 +1,17 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include"heapSort.h"
+#include<time.h>
 
 // Exemplo de uso
 int main() {
 
     int arr[] = {12, 11, 13, 5, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
+    clock_t inicio, fim;
+    double tempo_de_uso_CPU;
+
+    inicio = clock(); 
 
     printf("Vetor original:\n");
     imprimirArray(arr, n);
@@ -15,6 +20,13 @@ int main() {
 
     printf("Vetor ordenado usando o Heap Sort:\n");
     imprimirArray(arr, n);
+
+    // Captura o tempo de término
+		fim = clock();
+
+		tempo_de_uso_CPU = ((double) (fim - inicio)) / CLOCKS_PER_SEC; // Calcula o tempo de execução em segundos
+
+		printf("Tempo de execução: %.8f segundos\n", tempo_de_uso_CPU);
 
     return 0;
 }
